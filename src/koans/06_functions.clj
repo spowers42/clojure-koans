50150c94482e1dc072c544ce2a23bf90a474f492
Scott <spowers.42@gmail.com>
2014-07-04 09:04:04 -0400

    solved more

--- src/koans/06_functions.clj
+++ src/koans/06_functions.clj
@@ -30,11 +30,11 @@
 
   "Functions can also take other functions as input"
   (= 20 ((fn [f] (f 4 5))
-           ___))
+           #(* %1 %2)))
 
   "Higher-order functions take function arguments"
-  (= 25 (___
+  (= 25 (#(%1 5)
           (fn [n] (* n n))))
 
   "But they are often better written using the names of functions"
-  (= 25 (___ square)))
+  (= 25 (#(%1 5) square)))
